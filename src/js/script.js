075ea4f9167f2f99cac55ae956687826e7f1bb06
Anti-Shperml <59616866+Anti-Shperml@users.noreply.github.com>
2022-09-10 22:41:21 +0700

    basic scripts + jquery connect

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,36 @@
-function switchScreen(elID){ // Изменение отображение форм авторизации и регистрации
-    let reg = document.getElementById("reg__page"); // окно регистрации
-    let auth = document.getElementById("auth__page"); // окно авторизации
-    if(elID == 0){ 
-        auth.classList.add("hide"); // Скрыть авторизацию
-        reg.classList.remove("hide"); // Показать регистрацию
+$(function(){
+    var wrapper_childs = [$(".reg"), $(".auth"), $(".profile"), $(".cart")];
+    var wrapper = $("#wrapper");
+    wrapper.hide();
+
+    function hideWrapChilds(){
+        wrapper_childs.forEach(e => {
+            e.hide();
+            console.log(e.length);
+        });
     }
-    else{
-        reg.classList.add("hide"); // Скрыть регистрацию
-        auth.classList.remove("hide") // Показать авторизацию
-    }
-}
-function openProfile(){ // Открытие окна профиля
-    let profile = document.getElementById("profile_div"); // блок профиля
-    let nav_item = document.getElementById("prof__outer"); // блок пункта меню
-    nav_item.classList.add("active"); // добавление класса пункту меню
-    profile.classList.remove("hide"); // удаление класса блоку профиля
-}
-function closeProfile(){ // Скрытие окна профиля
-    let profile = document.getElementById("profile_div"); // блок профиля
-    let nav_item = document.getElementById("prof__outer"); // блок пункта меню
-    nav_item.classList.remove("active"); // удаление класса пункту меню
-    profile.classList.add("hide"); // добавление класса блоку профиля
-}
-if(el = document.getElementById("profile_text")){ // Проверка на существование блока профиля
-    el.addEventListener("click", function(e){ // добавление слушателя событий
-        e.preventDefault(); // отключение действия "по умолчанию"
-        openProfile(); // вызов функции "открытие профиля"
+
+    hideWrapChilds();
+
+    $("#profile").on("click", function(){
+        if(wrapper_childs[2].length != 0){
+            $(".auth").show();
+        }
+        else{
+            $(".profile").show();
+        }
+        wrapper.show(500);
     });
-}
+    $(".wrapper__header img").on("click", function(){
+        wrapper.hide(500);
+        hideWrapChilds();
+    });
+    $(".busket__button").on("click", function(){
+        $(".cart").show();
+        wrapper.show(500);
+    })
+    $(".switch__form").on("click", function(){
+        $(".reg").toggle();
+        $(".auth").toggle();
+    })
+})
