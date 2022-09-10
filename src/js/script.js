12f4b2760cba8087395d0f64670484aa361b934a
Anti-Shperml <59616866+Anti-Shperml@users.noreply.github.com>
2022-09-11 00:03:19 +0700

    +notification & promocode

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,18 @@ $(function(){
     var wrapper_childs = [$(".reg"), $(".auth"), $(".profile"), $(".cart")];
     var wrapper = $("#wrapper");
     wrapper.hide();
+    $(".notification").hide();
 
     function hideWrapChilds(){
         wrapper_childs.forEach(e => {
             e.hide();
-            console.log(e.length);
         });
     }
 
     hideWrapChilds();
 
     $("#profile").on("click", function(){
+        // Открытие профиля по нажатию пункта меню
         if(wrapper_childs[2].length != 0){
             $(".auth").show();
         }
@@ -21,16 +22,57 @@ $(function(){
         }
         wrapper.show(500);
     });
+
     $(".wrapper__header img").on("click", function(){
+        // Закрытие менюшек, появляющихся на переднем плане, при нажатии на крест
         wrapper.hide(500);
         hideWrapChilds();
     });
+
     $(".busket__button").on("click", function(){
+        // Открытие корзины
         $(".cart").show();
         wrapper.show(500);
-    })
+    });
+
     $(".switch__form").on("click", function(){
+        // Смена окна регистрации на окно авторизации и наоборот при нажатии на кнопку
         $(".reg").toggle();
         $(".auth").toggle();
+    });
+
+    $("#get_promocode").on("click", function(){
+        // Копирование промокода при нажатии на кнопку
+        var $tmp = $("<input>");
+        $("body").append($tmp);
+        $tmp.val("FIFTYCENT").select();
+        document.execCommand("copy");
+        $tmp.remove();
+        notification(1, "Промокод успешно скопирован в Буфер обмена")
     })
+
+    function notification(type, description){
+        // Уведомление
+        let header, color;
+        switch(type){
+            case 0:
+                header = "Информация";
+                color = "#4084c4";
+                break;
+            case 1:
+                header = "Успешно";
+                color = "#40c46c";
+                break;
+            case 2:
+                header = "Ошибка";
+                color = "#c44040";
+                break;
+
+        }
+        $(".notification").css("border-color", color);
+        $("#not__header")[0].innerHTML = header;
+        $("#not__desc")[0].innerHTML = description;
+        $(".notification").show(500).delay(3000).hide(500);
+    };
+
 })
